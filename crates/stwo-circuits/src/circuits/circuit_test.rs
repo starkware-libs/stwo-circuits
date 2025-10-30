b04e4fc54de9d1f6348776573743fb5b800be6e6
Lior Goldberg <lior@starkware.co>
2025-10-30 16:56:32 +0200

    Add Circuit struct.
    commit-id:e3a2443b

---
+++ crates/stwo-circuits/src/circuits/circuit_test.rs
@@ -0,0 +1,45 @@
+use crate::circuits::circuit::{Add, Circuit, Eq, Mul, Sub};
+
+#[test]
+fn circuit_add() {
+    let mut c = Circuit::default();
+    c.add.push(Add { in0: 0, in1: 1, out: 2 });
+    c.check(&[1.into(), 2.into(), 3.into()]).unwrap();
+    assert_eq!(
+        c.check(&[1.into(), 2.into(), 4.into()]).unwrap_err(),
+        "(3 + 0i) + (0 + 0i)u != (4 + 0i) + (0 + 0i)u"
+    );
+}
+
+#[test]
+fn circuit_sub() {
+    let mut c = Circuit::default();
+    c.sub.push(Sub { in0: 0, in1: 1, out: 3 });
+    c.check(&[10.into(), 7.into(), 0.into(), 3.into()]).unwrap();
+    assert_eq!(
+        c.check(&[10.into(), 7.into(), 0.into(), 4.into()]).unwrap_err(),
+        "(3 + 0i) + (0 + 0i)u != (4 + 0i) + (0 + 0i)u"
+    );
+}
+
+#[test]
+fn circuit_mul() {
+    let mut c = Circuit::default();
+    c.mul.push(Mul { in0: 1, in1: 1, out: 2 });
+    c.check(&[1.into(), 4.into(), 16.into()]).unwrap();
+    assert_eq!(
+        c.check(&[1.into(), 5.into(), 16.into()]).unwrap_err(),
+        "(25 + 0i) + (0 + 0i)u != (16 + 0i) + (0 + 0i)u"
+    );
+}
+
+#[test]
+fn circuit_eq() {
+    let mut c = Circuit::default();
+    c.eq.push(Eq { in0: 1, in1: 2 });
+    c.check(&[1.into(), 2.into(), 2.into()]).unwrap();
+    assert_eq!(
+        c.check(&[1.into(), 1.into(), 2.into()]).unwrap_err(),
+        "(1 + 0i) + (0 + 0i)u != (2 + 0i) + (0 + 0i)u"
+    );
+}
