9d43c9841e1708c554e316b5c76c5ce5bc5c7c8d
Lior Goldberg <lior@starkware.co>
2025-11-01 14:50:49 +0200

    Add simple_air example.
    commit-id:436e8361

---
+++ crates/stwo-circuits/src/examples/simple_air_test.rs
@@ -0,0 +1,24 @@
+use stwo::core::air::Component;
+use stwo::core::channel::Blake2sM31Channel;
+use stwo::core::pcs::{CommitmentSchemeVerifier, PcsConfig};
+use stwo::core::vcs::blake2_merkle::Blake2sM31MerkleChannel;
+use stwo::core::verifier::verify;
+
+use crate::examples::simple_air::create_proof;
+
+#[test]
+fn verify_simple_proof() {
+    let config = PcsConfig::default();
+    let (component, proof) = create_proof();
+
+    // Verify.
+    let verifier_channel = &mut Blake2sM31Channel::default();
+    let commitment_scheme = &mut CommitmentSchemeVerifier::<Blake2sM31MerkleChannel>::new(config);
+
+    // Retrieve the expected column sizes in each commitment interaction, from the AIR.
+    let sizes = component.trace_log_degree_bounds();
+    commitment_scheme.commit(proof.proof.commitments[0], &sizes[0], verifier_channel);
+    commitment_scheme.commit(proof.proof.commitments[1], &sizes[1], verifier_channel);
+    commitment_scheme.commit(proof.proof.commitments[2], &sizes[2], verifier_channel);
+    verify(&[&component], verifier_channel, commitment_scheme, proof.proof).unwrap();
+}
