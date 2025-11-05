732443b7a093811132376e29ab87715ac3b03b36
Lior Goldberg <lior@starkware.co>
2025-11-05 23:50:56 +0200

    Add draw_qm31 and draw_two_qm31s.
    commit-id:30794d07

--- crates/stwo-circuits/src/stark_verifier/verify.rs
+++ crates/stwo-circuits/src/stark_verifier/verify.rs
@@ -15,5 +15,17 @@ pub fn verify(context: &mut Context<impl IValue>, proof: &Proof<Var>, _config: &
     channel.mix_commitment(context, proof.preprocessed_root);
     channel.mix_commitment(context, proof.trace_root);
 
+    // TODO(lior): Add proof of work before drawing the interaction elements.
+
+    // Pick the interaction elements.
+    let [_interaction_z, _interaction_alpha] = channel.draw_two_qm31s(context);
+
+    channel.mix_commitment(context, proof.interaction_root);
+
+    // Draw a random QM31 coefficient for the composition polynomial.
+    let _composition_polynomial_coef = channel.draw_qm31(context);
+
+    channel.mix_commitment(context, proof.composition_polynomial_root);
+
     // TODO(lior): Complete the verification.
 }
