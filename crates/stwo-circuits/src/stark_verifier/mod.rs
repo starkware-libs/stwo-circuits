3b0c9b4b7bed895f9966445bd198929f90252efe
Lior Goldberg <lior@starkware.co>
2025-11-06 02:21:12 +0200

    Add extract_expected_composition_eval().
    commit-id:4e92cc3a

--- crates/stwo-circuits/src/stark_verifier/mod.rs
+++ crates/stwo-circuits/src/stark_verifier/mod.rs
@@ -1,4 +1,5 @@
 pub mod channel;
+pub mod circle;
 pub mod oods;
 pub mod proof;
 pub mod proof_from_stark_proof;
--- crates/stwo-circuits/src/stark_verifier/verify.rs
+++ crates/stwo-circuits/src/stark_verifier/verify.rs
@@ -1,14 +1,14 @@
 use crate::circuits::context::{Context, Var};
 use crate::circuits::ivalue::IValue;
-
 use crate::stark_verifier::channel::Channel;
+use crate::stark_verifier::oods::extract_expected_composition_eval;
 use crate::stark_verifier::proof::{Proof, ProofConfig};
 
 #[cfg(test)]
 #[path = "verify_test.rs"]
 pub mod test;
 
-pub fn verify(context: &mut Context<impl IValue>, proof: &Proof<Var>, _config: &ProofConfig) {
+pub fn verify(context: &mut Context<impl IValue>, proof: &Proof<Var>, config: &ProofConfig) {
     let mut channel = Channel::new(context);
 
     // Mix the trace commitments into the channel.
@@ -28,7 +28,14 @@ pub fn verify(context: &mut Context<impl IValue>, proof: &Proof<Var>, _config: &
     channel.mix_commitment(context, proof.composition_polynomial_root);
 
     // Draw a random point for the OODS.
-    let _oods_point = channel.draw_point(context);
+    let oods_point = channel.draw_point(context);
+
+    let _expected_composition_eval = extract_expected_composition_eval(
+        context,
+        &proof.composition_eval_at_oods,
+        oods_point,
+        config.log_evaluation_domain_size(),
+    );
 
     // TODO(lior): Complete the verification.
 }
