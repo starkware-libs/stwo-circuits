79bbc9b17a203c3a06b9da8b631d775193f9f8ce
Lior Goldberg <lior@starkware.co>
2025-11-09 15:13:10 +0200

    Add OODS check to verify().
    commit-id:16165a1c

---
+++ crates/stwo-circuits/src/stark_verifier/statement.rs
@@ -0,0 +1,27 @@
+use stwo::core::circle::CirclePoint;
+
+use crate::circuits::context::{Context, Var};
+use crate::circuits::ivalue::IValue;
+use crate::stark_verifier::proof::InteractionAtOods;
+
+/// Values at the OODS point (and its previous point where applicable).
+pub struct OodsSamples<'a> {
+    pub preprocessed_columns: &'a [Var],
+    pub trace: &'a [Var],
+    pub interaction: &'a InteractionAtOods<Var>,
+}
+
+/// Represents an AIR and its public inputs.
+pub trait Statement {
+    /// Evaluates the composition polynomial at the OODS point (after dividing by the domain
+    /// polynomial).
+    fn evaluate(
+        &self,
+        context: &mut Context<impl IValue>,
+        oods_samples: OodsSamples<'_>,
+        pt: CirclePoint<Var>,
+        log_domain_size: usize,
+        composition_polynomial_coef: Var,
+        interaction_elements: [Var; 2],
+    ) -> Var;
+}
