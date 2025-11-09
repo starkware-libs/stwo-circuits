ad31e66bf4c7dbcf82b4a7bbe865b79668a203c6
Lior Goldberg <lior@starkware.co>
2025-11-09 18:29:53 +0200

    Add fri_commit().
    commit-id:da0f7a5a

---
+++ crates/stwo-circuits/src/stark_verifier/fri.rs
@@ -0,0 +1,24 @@
+use crate::circuits::context::{Context, Var};
+use crate::circuits::ivalue::IValue;
+use crate::stark_verifier::channel::Channel;
+use crate::stark_verifier::fri_proof::FriCommitProof;
+
+#[cfg(test)]
+#[path = "fri_test.rs"]
+pub mod test;
+
+// Commits to the FRI layers and returns the random alphas.
+pub fn fri_commit(
+    context: &mut Context<impl IValue>,
+    channel: &mut Channel,
+    proof: &FriCommitProof<Var>,
+) -> Vec<Var> {
+    let mut alphas = Vec::new();
+    for root in &proof.roots {
+        channel.mix_commitment(context, *root);
+        alphas.push(channel.draw_qm31(context));
+    }
+    channel.mix_qm31s(context, proof.last_layer_coefs.iter().cloned());
+
+    alphas
+}
