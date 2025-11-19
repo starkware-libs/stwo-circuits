bf2a01cab30978ec7bc230f7ef9f65eafbaf9bdd
Lior Goldberg <lior@starkware.co>
2025-11-19 19:19:19 +0200

    Add add_points().
    commit-id:6bc4e1c1

---
+++ crates/stwo-circuits/src/stark_verifier/circle_test.rs
@@ -0,0 +1,63 @@
+use num_traits::Zero;
+use stwo::core::circle::CirclePoint;
+use stwo::core::fields::cm31::CM31;
+use stwo::core::fields::m31::M31;
+use stwo::core::fields::qm31::QM31;
+
+use crate::circuits::context::TraceContext;
+use crate::circuits::ops::Guess;
+use crate::circuits::test_utils::{packed_values, simd_from_u32s};
+use crate::stark_verifier::circle::{add_points, add_points_simd};
+
+#[test]
+fn test_add_points() {
+    let pt0 = CirclePoint::<QM31> { x: 102767539.into(), y: 739428083.into() };
+    let pt1 = CirclePoint::<QM31> { x: 946122697.into(), y: 337868966.into() };
+    let mut context = TraceContext::default();
+
+    let pt0_var = &pt0.guess(&mut context);
+    let pt1_var = &pt1.guess(&mut context);
+
+    let res = add_points(&mut context, pt0_var, pt1_var);
+
+    let expected_res = pt0 + pt1;
+
+    assert_eq!(context.get(res.x), expected_res.x);
+    assert_eq!(context.get(res.y), expected_res.y);
+
+    context.circuit.check(context.values()).unwrap();
+}
+
+#[test]
+fn test_add_points_simd() {
+    let pt0 = CirclePoint::<M31> { x: 102767539.into(), y: 739428083.into() };
+    let pt1 = CirclePoint::<M31> { x: 1562688784.into(), y: 946400219.into() };
+
+    let pt2 = CirclePoint::<M31> { x: 946122697.into(), y: 337868966.into() };
+    let pt3 = CirclePoint::<M31> { x: 2104020285.into(), y: 511427956.into() };
+    let mut context = TraceContext::default();
+
+    let first_points = CirclePoint {
+        x: simd_from_u32s(&mut context, vec![pt0.x.0, pt1.x.0]),
+        y: simd_from_u32s(&mut context, vec![pt0.y.0, pt1.y.0]),
+    };
+    let second_points = CirclePoint {
+        x: simd_from_u32s(&mut context, vec![pt2.x.0, pt3.x.0]),
+        y: simd_from_u32s(&mut context, vec![pt2.y.0, pt3.y.0]),
+    };
+    let res = add_points_simd(&mut context, &first_points, &second_points);
+
+    let pt0_plus_pt2 = pt0 + pt2;
+    let pt1_plus_pt3 = pt1 + pt3;
+
+    assert_eq!(
+        packed_values(&context, &res.x),
+        vec![QM31(CM31(pt0_plus_pt2.x, pt1_plus_pt3.x), CM31::zero())]
+    );
+    assert_eq!(
+        packed_values(&context, &res.y),
+        vec![QM31(CM31(pt0_plus_pt2.y, pt1_plus_pt3.y), CM31::zero())]
+    );
+
+    context.circuit.check(context.values()).unwrap();
+}
