db85cb7ef337174ea332c709da41b9309ca36027
Lior Goldberg <lior@starkware.co>
2025-11-22 18:47:14 +0200

    Add M31Wrapper.
    commit-id:fd7ddf48

--- crates/stwo-circuits/src/stark_verifier/oods.rs
+++ crates/stwo-circuits/src/stark_verifier/oods.rs
@@ -1,9 +1,11 @@
 use stwo::core::circle::CirclePoint;
+use stwo::core::fields::m31::M31;
+use stwo::core::fields::qm31::QM31;
 
 use crate::circuits::EXTENSION_DEGREE;
 use crate::circuits::context::{Context, Var};
-use crate::circuits::ivalue::IValue;
-use crate::circuits::ops::from_partial_evals;
+use crate::circuits::ivalue::{IValue, NoValue};
+use crate::circuits::ops::{Guess, from_partial_evals, pointwise_mul};
 use crate::eval;
 use crate::stark_verifier::circle::double_x;
 
@@ -13,6 +15,44 @@ pub const N_COMPOSITION_COLUMNS: usize = COMPOSITION_SPLIT * EXTENSION_DEGREE;
 #[cfg(test)]
 #[path = "oods_test.rs"]
 pub mod test;
+
+/// Represents a value that should be in the base field `M31`.
+///
+/// Using the [Guess] trait on [M31Wrapper] and gates that guarantee that the guessed value is
+/// indeed in the base field `M31`.
+#[derive(Clone, Debug)]
+pub struct M31Wrapper<T>(T);
+
+impl<T> M31Wrapper<T> {
+    #[expect(dead_code)]
+    fn get(&self) -> &T {
+        &self.0
+    }
+}
+
+impl From<M31> for M31Wrapper<QM31> {
+    fn from(value: M31) -> Self {
+        M31Wrapper(value.into())
+    }
+}
+
+impl From<NoValue> for M31Wrapper<NoValue> {
+    fn from(_: NoValue) -> Self {
+        M31Wrapper(NoValue)
+    }
+}
+
+impl<Value: IValue> Guess<Value> for M31Wrapper<Value> {
+    type Target = M31Wrapper<Var>;
+
+    fn guess(&self, context: &mut Context<Value>) -> Self::Target {
+        let value = self.0.guess(context);
+        // Mask the value with `1 + 0 * i + 0 * u + 0 * iu` to ensure (in the circuit) it is
+        // in the base field `M31`.
+        let masked_value = pointwise_mul(context, value, context.one());
+        M31Wrapper(masked_value)
+    }
+}
 
 /// Computes the expected value of the composition polynomial at the OODS point, based on the
 /// broken composition polynomial commitment.
--- crates/stwo-circuits/src/stark_verifier/oods_test.rs
+++ crates/stwo-circuits/src/stark_verifier/oods_test.rs
@@ -1,10 +1,32 @@
+use expect_test::expect;
 use stwo::core::circle::CirclePoint;
 
-use crate::circuits::context::TraceContext;
+use crate::circuits::context::{Context, TraceContext};
+use crate::circuits::ivalue::{NoValue, qm31_from_u32s};
 use crate::circuits::ops::Guess;
-use crate::stark_verifier::oods::extract_expected_composition_eval;
-// use crate::stark_verifier::circle::CirclePoint;
-use crate::circuits::ivalue::qm31_from_u32s;
+use crate::stark_verifier::oods::{M31Wrapper, extract_expected_composition_eval};
+
+#[test]
+fn test_m31_wrapper_guess_circuit() {
+    let mut context = Context::<NoValue>::default();
+    let res = M31Wrapper::from(NoValue).guess(&mut context);
+    expect!["M31Wrapper([3])"].assert_eq(&format!("{res:?}"));
+    expect![[r#"
+        {
+            (0 + 0i) + (0 + 0i)u: [0],
+            (1 + 0i) + (0 + 0i)u: [1],
+        }
+    "#]]
+    .assert_debug_eq(&context.constants());
+    expect![[r#"
+        [0] = [0] + [0]
+        [1] = [1] + [0]
+        [2] = [2] + [0]
+        [3] = [2] x [1]
+
+    "#]]
+    .assert_debug_eq(&context.circuit);
+}
 
 #[test]
 fn extract_expected_composition_eval_regression() {
