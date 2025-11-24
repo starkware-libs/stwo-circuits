465c901a7cfcf50dfb3b9f39ff97b2277d78ab2d
Gali Michlevich <gali@starkware.co>
2025-11-24 19:45:50 +0200

    Fibonacci Circuit

--- crates/stwo-circuits/src/circuit_prover/fibonacci_prover.rs
+++ crates/stwo-circuits/src/circuit_prover/fibonacci_prover.rs
@@ -1,9 +1,24 @@
-use crate::circuits::context::Context;
+use crate::circuits::{context::Context, ops::guess};
+use crate::eval;
+use num_traits::{One, Zero};
 use stwo::core::fields::qm31::QM31;
+
+const N: usize = 1024;
 
 #[cfg(test)]
 #[path = "fibonacci_prover_test.rs"]
 pub mod test;
+
+pub fn build_fibonacci_circuit() -> Context<QM31> {
+    let mut context = Context::<QM31>::default();
+
+    let (mut a, mut b) = (guess(&mut context, QM31::zero()), guess(&mut context, QM31::one()));
+    for _ in 2..N {
+        (a, b) = (b, eval!(&mut context, (a) + (b)));
+    }
+
+    context
+}
 
 /// Proves a Fibonacci circuit that computes the N-th Fibonacci number.
 /// a_0 = 0, a_1 = 1, a_n = a_{n-1} + a_{n-2}.
--- crates/stwo-circuits/src/circuit_prover/fibonacci_prover_test.rs
+++ crates/stwo-circuits/src/circuit_prover/fibonacci_prover_test.rs
@@ -1,7 +1,16 @@
-use crate::{circuit_prover::fibonacci_prover::prove_fibonacci, circuits::context::Context};
+use expect_test::expect;
+
+use crate::circuit_prover::fibonacci_prover::{N, build_fibonacci_circuit, prove_fibonacci};
 
 #[test]
 fn test_prove_fibonacci() {
-    let fibonacci_circuit = Context::default();
+    let fibonacci_circuit = build_fibonacci_circuit();
+
+    assert_eq!(fibonacci_circuit.values().len(), N + 2);
+    expect![[r#"
+        (980146853 + 0i) + (0 + 0i)u
+    "#]]
+    .assert_debug_eq(&fibonacci_circuit.values().last());
+
     prove_fibonacci(fibonacci_circuit);
 }
