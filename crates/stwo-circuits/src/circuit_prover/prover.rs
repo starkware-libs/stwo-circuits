9b25b925cae5deb7d6839333963a7908cc5cef4a
Gali Michlevich <gali@starkware.co>
2025-11-24 19:45:50 +0200

    Fibonacci Context Test

---
+++ crates/stwo-circuits/src/circuit_prover/prover.rs
@@ -0,0 +1,10 @@
+use crate::circuits::context::Context;
+use stwo::core::fields::qm31::QM31;
+
+#[cfg(test)]
+#[path = "prover_test.rs"]
+pub mod test;
+
+pub fn prove(mut _context: Context<QM31>) {
+    // TODO(Gali): Implement.
+}
