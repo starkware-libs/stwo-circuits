1deb3120d2c2596143cc005dfd09f91c2eb32743
Lior Goldberg <lior@starkware.co>
2025-11-28 21:50:57 +0200

    Add compute_fri_input().
    commit-id:11505047

--- crates/stwo-circuits/src/stark_verifier/oods_test.rs
+++ crates/stwo-circuits/src/stark_verifier/oods_test.rs
@@ -1,10 +1,17 @@
 use expect_test::expect;
 use stwo::core::circle::CirclePoint;
+use stwo::core::fields::m31::M31;
 
 use crate::circuits::context::{Context, TraceContext};
 use crate::circuits::ivalue::{NoValue, qm31_from_u32s};
 use crate::circuits::ops::Guess;
-use crate::stark_verifier::oods::{empty_eval_domain_samples, extract_expected_composition_eval};
+use crate::circuits::test_utils::simd_from_u32s;
+use crate::circuits::wrappers::M31Wrapper;
+use crate::stark_verifier::oods::{
+    EvalDomainSamples, OodsResponse, compute_fri_input, empty_eval_domain_samples,
+    extract_expected_composition_eval,
+};
+use crate::stark_verifier::select_queries::select_queries;
 
 #[test]
 fn test_eval_domain_samples_guess_circuit() {
@@ -62,6 +69,67 @@ fn extract_expected_composition_eval_regression() {
         context.get(expected_composition_eval),
         qm31_from_u32s(443798542, 633915785, 595028408, 165661052)
     );
+
+    context.validate_circuit();
+}
+
+#[test]
+fn test_compute_fri_input_regression() {
+    let pt = CirclePoint {
+        x: qm31_from_u32s(1977529453, 822446523, 1665855107, 812402677),
+        y: qm31_from_u32s(310991198, 1931472985, 1200685911, 1588389778),
+    };
+
+    let mut context = TraceContext::default();
+    let pt_var = pt.guess(&mut context);
+
+    let oods_responses = vec![
+        OodsResponse {
+            trace_idx: 0,
+            column_idx: 0,
+            pt: pt_var,
+            value: context.new_var(qm31_from_u32s(1, 0, 0, 0)),
+        },
+        OodsResponse {
+            trace_idx: 0,
+            column_idx: 1,
+            pt: pt_var,
+            value: context.new_var(qm31_from_u32s(2065172982, 64209128, 2018861108, 1995226139)),
+        },
+        OodsResponse {
+            trace_idx: 0,
+            column_idx: 2,
+            pt: pt_var,
+            value: context.new_var(qm31_from_u32s(2038440027, 1469156040, 504751706, 1024643555)),
+        },
+    ];
+
+    let input = simd_from_u32s(&mut context, vec![0, 21, 26]);
+    let log_domain_size = 5;
+    let queries = select_queries(&mut context, &input, log_domain_size);
+
+    let trace_queries_vars: Vec<Vec<M31>> = vec![
+        vec![1.into(), 863170483.into(), 1430398088.into()],
+        vec![1.into(), 58834968.into(), 1532221375.into()],
+        vec![1.into(), 1606816039.into(), 264974634.into()],
+    ];
+    let trace_queries = EvalDomainSamples {
+        data: vec![
+            trace_queries_vars
+                .iter()
+                .map(|v| v.iter().map(|v2| M31Wrapper::from(*v2).guess(&mut context)).collect())
+                .collect(),
+        ],
+    };
+
+    let alpha = context.new_var(qm31_from_u32s(1058706599, 1486409878, 1052004241, 54096853));
+
+    let res = compute_fri_input(&mut context, &oods_responses, &queries, &trace_queries, alpha);
+
+    assert_eq!(res.len(), 3);
+    assert_eq!(context.get(res[0]), qm31_from_u32s(1799948512, 769698546, 2025315394, 642248561));
+    assert_eq!(context.get(res[1]), qm31_from_u32s(628725928, 1812037401, 1687426883, 1599536169));
+    assert_eq!(context.get(res[2]), qm31_from_u32s(2027161127, 718656514, 538553980, 959876533));
 
     context.validate_circuit();
 }
