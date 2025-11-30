10730d5a51ef407e5c01b1816d756d5422381ada
liorgold2 <38202661+liorgold2@users.noreply.github.com>
2025-11-30 19:53:43 +0200

    Add hash_node(). (#58)
    commit-id:4779268e

--- crates/stwo-circuits/src/stark_verifier/merkle_test.rs
+++ crates/stwo-circuits/src/stark_verifier/merkle_test.rs
@@ -1,6 +1,7 @@
+use crate::circuits::blake::HashValue;
 use crate::circuits::context::TraceContext;
 use crate::circuits::ivalue::qm31_from_u32s;
-use crate::stark_verifier::merkle::hash_leaf_m31s;
+use crate::stark_verifier::merkle::{hash_leaf_m31s, hash_node};
 
 #[test]
 fn hash_leaf_m31s_regression() {
@@ -19,6 +20,27 @@ fn hash_leaf_m31s_regression() {
 
     assert_eq!(context.get(hash.0), qm31_from_u32s(483650195, 1143215778, 1399105963, 121243225));
     assert_eq!(context.get(hash.1), qm31_from_u32s(1343116297, 264974384, 1201369425, 1524730384));
+
+    context.validate_circuit();
+}
+
+#[test]
+fn hash_node_regression() {
+    let mut context = TraceContext::default();
+
+    let left = HashValue(
+        context.new_var(qm31_from_u32s(1206199574, 725559475, 484842011, 871283881)),
+        context.new_var(qm31_from_u32s(1827188342, 1597668943, 763527182, 238830106)),
+    );
+    let right = HashValue(
+        context.new_var(qm31_from_u32s(314780017, 161087059, 1415631711, 1712686715)),
+        context.new_var(qm31_from_u32s(873946371, 993675704, 1750257287, 1496441219)),
+    );
+
+    let hash = hash_node(&mut context, left, right);
+
+    assert_eq!(context.get(hash.0), qm31_from_u32s(1290083578, 670256590, 203247471, 492011214));
+    assert_eq!(context.get(hash.1), qm31_from_u32s(353269841, 1619070080, 770215254, 1663098736));
 
     context.validate_circuit();
 }
