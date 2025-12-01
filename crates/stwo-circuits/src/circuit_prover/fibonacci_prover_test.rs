178d777b90f6dd31c6ac25fa2d7f0c33cb9b2501
Gali-StarkWare <gali@starkware.co>
2025-12-01 17:41:53 +0200

    Fibonacci Circuit (#50)
    <!-- Reviewable:start -->
    This change is [<img src="https://reviewable.io/review_button.svg" height="34" align="absmiddle" alt="Reviewable"/>](https://reviewable.io/reviews/starkware-libs/stwo-circuits/50)
    <!-- Reviewable:end -->

--- crates/stwo-circuits/src/circuit_prover/fibonacci_prover_test.rs
+++
@@ -1,7 +0,0 @@
-use crate::{circuit_prover::fibonacci_prover::prove_fibonacci, circuits::context::Context};
-
-#[test]
-fn test_prove_fibonacci() {
-    let fibonacci_circuit = Context::default();
-    prove_fibonacci(fibonacci_circuit);
-}
---
+++ crates/stwo-circuits/src/circuit_prover/prover_test.rs
@@ -0,0 +1,31 @@
+use crate::circuit_prover::prover::prove;
+use crate::circuits::{context::Context, ops::guess};
+use crate::eval;
+use expect_test::expect;
+use num_traits::{One, Zero};
+use stwo::core::fields::qm31::QM31;
+
+const N: usize = 1024;
+
+pub fn build_fibonacci_context() -> Context<QM31> {
+    let mut context = Context::<QM31>::default();
+
+    let (mut a, mut b) = (guess(&mut context, QM31::zero()), guess(&mut context, QM31::one()));
+    for _ in 2..N {
+        (a, b) = (b, eval!(&mut context, (a) + (b)));
+    }
+
+    expect![[r#"
+        (980146853 + 0i) + (0 + 0i)u
+    "#]]
+    .assert_debug_eq(&context.get(b));
+
+    context
+}
+
+#[test]
+fn test_prove_fibonacci() {
+    let fibonacci_context = build_fibonacci_context();
+    fibonacci_context.validate_circuit();
+    prove(fibonacci_context);
+}
