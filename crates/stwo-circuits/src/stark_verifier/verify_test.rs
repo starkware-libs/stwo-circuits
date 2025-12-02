78615232d901d76aeee7b7e487fd061ef88c090e
Lior Goldberg <lior@starkware.co>
2025-12-02 10:33:58 +0200

    Test that verify() checks the Merkle decommitment.
    commit-id:833bbba7

--- crates/stwo-circuits/src/stark_verifier/verify_test.rs
+++ crates/stwo-circuits/src/stark_verifier/verify_test.rs
@@ -1,3 +1,6 @@
+use rstest::rstest;
+use stwo::core::vcs::blake2_hash::Blake2sHash;
+
 use crate::circuits::context::{Context, TraceContext};
 use crate::circuits::ivalue::NoValue;
 use crate::circuits::ops::Guess;
@@ -8,8 +11,17 @@ use crate::stark_verifier::proof::{ProofConfig, empty_proof};
 use crate::stark_verifier::proof_from_stark_proof::proof_from_stark_proof;
 use crate::stark_verifier::verify::verify;
 
-#[test]
-fn test_verify() {
+enum ProofModifier {
+    /// Keep the proof unchanged.
+    None,
+    /// Modify an element of the authentication path for one of the traces.
+    WrongTraceAuthPath,
+}
+
+#[rstest]
+#[case::success(ProofModifier::None)]
+#[case::wrong_trace_auth_path(ProofModifier::WrongTraceAuthPath)]
+fn test_verify(#[case] proof_modifier: ProofModifier) {
     let config = ProofConfig {
         n_proof_of_work_bits: 10,
         n_preprocessed_columns: 1,
@@ -33,7 +45,16 @@ fn test_verify() {
     };
 
     // Create a context with values from the proof.
-    let (_component, proof) = create_proof();
+    let (_component, mut proof) = create_proof();
+    match proof_modifier {
+        ProofModifier::None => {}
+        ProofModifier::WrongTraceAuthPath => {
+            let first_query = proof.aux.unsorted_query_locations[0];
+            let first_layer_values = &mut proof.aux.trace_decommitment[1].all_node_values[1];
+            let value: &mut Blake2sHash = first_layer_values.get_mut(&(first_query ^ 1)).unwrap();
+            value.0[0] ^= 1;
+        }
+    }
     let mut context = TraceContext::default();
     let proof = proof_from_stark_proof(&proof, &config);
     let proof_vars = proof.guess(&mut context);
@@ -42,7 +63,19 @@ fn test_verify() {
     // Make sure we got the same circuit.
     assert_eq!(context.circuit, novalue_circuit);
 
-    novalue_circuit.check(context.values()).unwrap();
+    let result = novalue_circuit.check(context.values());
+    match proof_modifier {
+        ProofModifier::None => {
+            result.unwrap();
+        }
+        ProofModifier::WrongTraceAuthPath => {
+            let err = result.unwrap_err();
+            // The error should be when comparing the main trace root.
+            let expected_value = context.get(proof_vars.trace_root.0);
+            assert!(err.contains(&expected_value.to_string()));
+        }
+    }
+
     novalue_circuit.check_yields();
     println!("Stats: {:?}", context.stats);
 }
