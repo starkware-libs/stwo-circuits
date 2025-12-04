68d8ba4db639504f7bdc0c2c0cffde1f4ffe1039
Ilya Lesokhin <ilya@starkware.co>
2025-12-04 13:56:15 +0200

    Add config.cumulative_sum_columns.

--- crates/stwo-circuits/src/stark_verifier/verify_test.rs
+++ crates/stwo-circuits/src/stark_verifier/verify_test.rs
@@ -35,6 +35,7 @@ fn test_verify(#[case] proof_modifier: ProofModifier) {
         n_preprocessed_columns: 1,
         n_trace_columns: 4,
         n_interaction_columns: 4,
+        cumulative_sum_columns: vec![true; 4],
         fri: FriConfig {
             log_trace_size: LOG_N_INSTANCES.try_into().unwrap(),
             log_blowup_factor: 1,
