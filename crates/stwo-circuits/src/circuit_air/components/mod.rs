0313e10d187c2fdb71c8e9b1e62328fb039eaa8e
Gali Michlevich <gali@starkware.co>
2025-12-09 15:56:58 +0200

    Components

--- crates/stwo-circuits/src/circuit_air/components/mod.rs
+++ crates/stwo-circuits/src/circuit_air/components/mod.rs
@@ -1,9 +1,17 @@
 pub mod qm31_ops;
+use std::marker::PhantomData;
+
 use crate::circuit_air::relations;
+use itertools::chain;
 use num_traits::Zero;
+use stwo::core::air::Component;
 use stwo::core::channel::Channel;
 use stwo::core::fields::qm31::SecureField;
 use stwo::core::pcs::TreeVec;
+use stwo::prover::ComponentProver;
+use stwo::prover::backend::Backend;
+use stwo_constraint_framework::TraceLocationAllocator;
+use stwo_constraint_framework::preprocessed_columns::PreProcessedColumnId;
 
 pub struct CircuitClaim {
     pub qm31_ops: qm31_ops::Claim,
@@ -44,4 +52,44 @@ pub fn lookup_sum(interaction_claim: &CircuitInteractionClaim) -> SecureField {
     let mut sum = SecureField::zero();
     sum += interaction_claim.qm31_ops.claimed_sum;
     sum
+}
+
+pub struct CircuitComponents<B: Backend> {
+    pub qm31_ops: qm31_ops::Component,
+    _backend: PhantomData<B>,
+}
+impl<B: Backend> CircuitComponents<B>
+where
+    stwo_constraint_framework::FrameworkComponent<crate::circuit_air::components::qm31_ops::Eval>:
+        stwo::prover::ComponentProver<B>,
+{
+    pub fn new(
+        circuit_claim: &CircuitClaim,
+        interaction_elements: &CircuitInteractionElements,
+        interaction_claim: &CircuitInteractionClaim,
+        // Describes the structure of the preprocessed trace. Sensitive to order.
+        preprocessed_column_ids: &[PreProcessedColumnId],
+    ) -> Self {
+        let tree_span_provider =
+            &mut TraceLocationAllocator::new_with_preprocessed_columns(preprocessed_column_ids);
+
+        let qm31_ops_component = qm31_ops::Component::new(
+            tree_span_provider,
+            qm31_ops::Eval {
+                claim: circuit_claim.qm31_ops,
+                gate_lookup_elements: interaction_elements.gate.clone(),
+            },
+            interaction_claim.qm31_ops.claimed_sum,
+        );
+
+        Self { qm31_ops: qm31_ops_component, _backend: PhantomData }
+    }
+
+    pub fn provers(&self) -> Vec<&dyn ComponentProver<B>> {
+        chain!([&self.qm31_ops as &dyn ComponentProver<B>,]).collect()
+    }
+
+    pub fn components(&self) -> Vec<&dyn Component> {
+        self.provers().into_iter().map(|component| component as &dyn Component).collect()
+    }
 }
