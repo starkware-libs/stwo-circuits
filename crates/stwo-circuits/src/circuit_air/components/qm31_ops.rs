13b03175a42ba7352d39c613ad18a664b0e39b54
Gali Michlevich <gali@starkware.co>
2025-12-09 17:53:22 +0200

    Trace

---
+++ crates/stwo-circuits/src/circuit_air/components/qm31_ops.rs
@@ -0,0 +1,19 @@
+use stwo::core::channel::Channel;
+use stwo::core::fields::qm31::SECURE_EXTENSION_DEGREE;
+use stwo::core::pcs::TreeVec;
+
+pub const N_TRACE_COLUMNS: usize = 12;
+pub struct Claim {
+    pub log_size: u32,
+}
+impl Claim {
+    pub fn log_sizes(&self) -> TreeVec<Vec<u32>> {
+        let trace_log_sizes = vec![self.log_size; N_TRACE_COLUMNS];
+        let interaction_log_sizes = vec![self.log_size; SECURE_EXTENSION_DEGREE];
+        TreeVec::new(vec![vec![], trace_log_sizes, interaction_log_sizes])
+    }
+
+    pub fn mix_into(&self, channel: &mut impl Channel) {
+        channel.mix_u64(self.log_size as u64);
+    }
+}
