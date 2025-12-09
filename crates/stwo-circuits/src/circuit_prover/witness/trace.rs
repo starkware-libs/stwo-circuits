80ffd9566f1593eddbea7f8fadd4f0961c16cc37
Gali Michlevich <gali@starkware.co>
2025-12-09 18:03:25 +0200

    Interaction Trace

--- crates/stwo-circuits/src/circuit_prover/witness/trace.rs
+++ crates/stwo-circuits/src/circuit_prover/witness/trace.rs
@@ -1,4 +1,6 @@
 use crate::circuit_air::components::CircuitClaim;
+use crate::circuit_air::components::CircuitInteractionClaim;
+use crate::circuit_air::components::CircuitInteractionElements;
 use crate::circuit_prover::witness::components::qm31_ops;
 use crate::circuit_prover::witness::preprocessed::PreProcessedTrace;
 use stwo::core::fields::qm31::QM31;
@@ -10,8 +12,28 @@ pub fn write_trace(
     context_values: &[QM31],
     preprocessed_trace: &PreProcessedTrace,
     tree_builder: &mut TreeBuilder<'_, '_, SimdBackend, Blake2sM31MerkleChannel>,
-) -> CircuitClaim {
-    let qm31_ops_claim = qm31_ops::write_trace(context_values, preprocessed_trace, tree_builder);
+) -> (CircuitClaim, CircuitInteractionClaimGenerator) {
+    let (qm31_ops_claim, qm31_ops_interaction_claim_generator) =
+        qm31_ops::write_trace(context_values, preprocessed_trace, tree_builder);
 
-    CircuitClaim { qm31_ops: qm31_ops_claim }
+    (
+        CircuitClaim { qm31_ops: qm31_ops_claim },
+        CircuitInteractionClaimGenerator { qm31_ops: qm31_ops_interaction_claim_generator },
+    )
+}
+
+pub struct CircuitInteractionClaimGenerator {
+    pub qm31_ops: qm31_ops::InteractionClaimGenerator,
+    // ...
+}
+impl CircuitInteractionClaimGenerator {
+    pub fn write_interaction_trace(
+        self,
+        tree_builder: &mut impl TreeBuilder<SimdBackend>,
+        interaction_elements: &CircuitInteractionElements,
+    ) -> CircuitInteractionClaim {
+        let qm31_ops_interaction_claim =
+            self.qm31_ops.write_interaction_trace(tree_builder, &interaction_elements.gate);
+        CircuitInteractionClaim { qm31_ops: qm31_ops_interaction_claim }
+    }
 }
