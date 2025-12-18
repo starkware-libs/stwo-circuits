62b3a887a52b721d7636f836a745f06bc9b6b7c8
Ilya Lesokhin <ilya@starkware.co>
2025-12-18 15:35:21 +0200

    Change create_proof to support multiple components.

--- crates/stwo-circuits/src/examples/simple_air_test.rs
+++ crates/stwo-circuits/src/examples/simple_air_test.rs
@@ -1,6 +1,6 @@
 use stwo::core::air::Component;
 use stwo::core::channel::{Blake2sM31Channel, Channel};
-use stwo::core::pcs::{CommitmentSchemeVerifier, PcsConfig};
+use stwo::core::pcs::{CommitmentSchemeVerifier, PcsConfig, TreeVec};
 use stwo::core::vcs_lifted::blake2_merkle::Blake2sM31MerkleChannel;
 use stwo::core::verifier::verify;
 
@@ -9,17 +9,25 @@ use crate::examples::simple_air::create_proof;
 #[test]
 fn verify_simple_proof() {
     let config = PcsConfig::default();
-    let (component, proof) = create_proof();
+    let (components, claimed_sums, proof) = create_proof();
 
     // Verify.
     let verifier_channel = &mut Blake2sM31Channel::default();
     let commitment_scheme = &mut CommitmentSchemeVerifier::<Blake2sM31MerkleChannel>::new(config);
 
     // Retrieve the expected column sizes in each commitment interaction, from the AIR.
-    let sizes = component.trace_log_degree_bounds();
+    let sizes =
+        TreeVec::concat_cols(components.iter().map(|c| c.trace_log_degree_bounds()).into_iter());
+
     commitment_scheme.commit(proof.proof.commitments[0], &sizes[0], verifier_channel);
     commitment_scheme.commit(proof.proof.commitments[1], &sizes[1], verifier_channel);
-    verifier_channel.mix_felts(&[component.claimed_sum()]);
+    verifier_channel.mix_felts(&claimed_sums);
     commitment_scheme.commit(proof.proof.commitments[2], &sizes[2], verifier_channel);
-    verify(&[&component], verifier_channel, commitment_scheme, proof.proof).unwrap();
+    verify(
+        &components.iter().map(|c| c.as_ref()).collect::<Vec<&dyn Component>>(),
+        verifier_channel,
+        commitment_scheme,
+        proof.proof,
+    )
+    .unwrap();
 }
--- crates/stwo-circuits/src/stark_verifier/verify_test.rs
+++ crates/stwo-circuits/src/stark_verifier/verify_test.rs
@@ -55,7 +55,7 @@ fn test_verify(#[case] proof_modifier: ProofModifier) {
     };
 
     // Create a context with values from the proof.
-    let (component, mut proof) = create_proof();
+    let (_components, claimed_sums, mut proof) = create_proof();
     match proof_modifier {
         ProofModifier::None => {}
         ProofModifier::WrongTraceAuthPath => {
@@ -80,7 +80,6 @@ fn test_verify(#[case] proof_modifier: ProofModifier) {
         }
     }
     let mut context = TraceContext::default();
-    let claimed_sums = vec![component.claimed_sum()];
     let proof = proof_from_stark_proof(&proof, &config, claimed_sums);
     let proof_vars = proof.guess(&mut context);
     verify(&mut context, &proof_vars, &config, &SimpleStatement::default());
