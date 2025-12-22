1b0e61cf530587c10f02541765d93527baaee957
Gali Michlevich <gali@starkware.co>
2025-12-22 15:47:43 +0200

    Move Claims to Cairo Air Main Mod File

--- crates/stwo-circuits/src/circuit_air/components/mod.rs
+++ crates/stwo-circuits/src/circuit_air/components/mod.rs
@@ -2,72 +2,14 @@ pub mod eq;
 pub mod prelude;
 pub mod qm31_ops;
 
-use crate::circuit_air::relations;
 use itertools::chain;
-use num_traits::Zero;
 use stwo::core::air::Component;
-use stwo::core::channel::Channel;
-use stwo::core::fields::qm31::SecureField;
-use stwo::core::pcs::TreeVec;
 use stwo::prover::ComponentProver;
 use stwo::prover::backend::simd::SimdBackend;
 use stwo_constraint_framework::TraceLocationAllocator;
 use stwo_constraint_framework::preprocessed_columns::PreProcessedColumnId;
 
-pub struct CircuitClaim {
-    pub qm31_ops_log_size: u32,
-    pub eq_log_size: u32,
-    // ...
-}
-impl CircuitClaim {
-    pub fn mix_into(&self, channel: &mut impl Channel) {
-        channel.mix_u64(self.qm31_ops_log_size as u64);
-        channel.mix_u64(self.eq_log_size as u64);
-    }
-
-    /// Returns the log sizes of the components.
-    /// Does not include the preprocessed trace log sizes.
-    pub fn log_sizes(&self) -> TreeVec<Vec<u32>> {
-        let log_sizes_list =
-            vec![qm31_ops::log_sizes(self.qm31_ops_log_size), eq::log_sizes(self.eq_log_size)];
-
-        TreeVec::concat_cols(log_sizes_list.into_iter())
-    }
-}
-
-pub struct CircuitInteractionElements {
-    pub gate: relations::Gate,
-}
-impl CircuitInteractionElements {
-    pub fn draw(channel: &mut impl Channel) -> CircuitInteractionElements {
-        CircuitInteractionElements { gate: relations::Gate::draw(channel) }
-    }
-}
-
-pub struct CircuitInteractionClaim {
-    pub qm31_ops_claimed_sum: SecureField,
-    pub eq_claimed_sum: SecureField,
-    // ...
-}
-impl CircuitInteractionClaim {
-    pub fn mix_into(&self, channel: &mut impl Channel) {
-        channel.mix_felts(&[self.qm31_ops_claimed_sum]);
-        channel.mix_felts(&[self.eq_claimed_sum]);
-    }
-}
-
-pub fn lookup_sum(
-    _claim: &CircuitClaim,
-    _interaction_elements: &CircuitInteractionElements,
-    interaction_claim: &CircuitInteractionClaim,
-) -> SecureField {
-    let mut sum = SecureField::zero();
-
-    sum += interaction_claim.qm31_ops_claimed_sum;
-    sum += interaction_claim.eq_claimed_sum;
-
-    sum
-}
+use crate::circuit_air::{CircuitClaim, CircuitInteractionClaim, CircuitInteractionElements};
 
 pub struct CircuitComponents {
     pub qm31_ops: qm31_ops::Component,
