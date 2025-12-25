65b00c12f0b69cdc238b4c289cc6c004391da457
Gali-StarkWare <gali@starkware.co>
2025-12-25 19:12:48 +0200

    Interaction Trace (#85)
    <!-- Reviewable:start -->
    This change is [<img src="https://reviewable.io/review_button.svg" height="34" align="absmiddle" alt="Reviewable"/>](https://reviewable.io/reviews/starkware-libs/stwo-circuits/85)
    <!-- Reviewable:end -->

--- crates/stwo-circuits/src/circuit_air/components/mod.rs
+++ crates/stwo-circuits/src/circuit_air/components/mod.rs
@@ -1,5 +1,8 @@
 pub mod qm31_ops;
+use crate::circuit_air::relations;
+use num_traits::Zero;
 use stwo::core::channel::Channel;
+use stwo::core::fields::qm31::SecureField;
 
 pub struct CircuitClaim {
     pub qm31_ops: qm31_ops::Claim,
@@ -9,4 +12,30 @@ impl CircuitClaim {
         let Self { qm31_ops } = self;
         qm31_ops.mix_into(channel);
     }
+}
+
+pub struct CircuitInteractionElements {
+    pub gate: relations::Gate,
+}
+impl CircuitInteractionElements {
+    pub fn draw(channel: &mut impl Channel) -> CircuitInteractionElements {
+        CircuitInteractionElements { gate: relations::Gate::draw(channel) }
+    }
+}
+
+pub struct CircuitInteractionClaim {
+    pub qm31_ops: qm31_ops::InteractionClaim,
+}
+impl CircuitInteractionClaim {
+    pub fn mix_into(&self, channel: &mut impl Channel) {
+        let Self { qm31_ops } = self;
+        qm31_ops.mix_into(channel);
+    }
+}
+
+pub fn lookup_sum(interaction_claim: &CircuitInteractionClaim) -> SecureField {
+    let CircuitInteractionClaim { qm31_ops } = interaction_claim;
+    let mut sum = SecureField::zero();
+    sum += qm31_ops.claimed_sum;
+    sum
 }
