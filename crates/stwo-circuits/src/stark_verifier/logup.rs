0cdea49c832db4fd1bbe0326f97b68f67681d436
Gali Michlevich <gali@starkware.co>
2026-01-02 12:28:00 +0200

    Move LogUp Functions

---
+++ crates/stwo-circuits/src/stark_verifier/logup.rs
@@ -0,0 +1,30 @@
+use crate::circuits::context::Context;
+use crate::circuits::context::Var;
+use crate::circuits::ivalue::IValue;
+use crate::eval;
+
+/// Computes the denominator of a logup term.
+pub fn combine_term(
+    context: &mut Context<impl IValue>,
+    element: &[Var],
+    interaction_elements: [Var; 2],
+) -> Var {
+    let mut element_iter = element.iter().rev();
+    let mut value = *element_iter.next().unwrap();
+    for elm in element_iter {
+        value = eval!(context, (value) * (interaction_elements[1]));
+        value = eval!(context, (value) + (*elm));
+    }
+    eval!(context, (value) - (interaction_elements[0]))
+}
+
+/// Computes the constraint polynomial for a single logup term.
+pub fn single_logup_term(
+    context: &mut Context<impl IValue>,
+    element: &[Var],
+    shifted_diff: Var,
+    interaction_elements: [Var; 2],
+) -> Var {
+    let denominator = combine_term(context, element, interaction_elements);
+    eval!(context, ((shifted_diff) * (denominator)) - (1))
+}
