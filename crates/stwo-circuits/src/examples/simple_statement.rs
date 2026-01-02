fd965240a1ff6d852ec739b77fc7a6aec6c86051
Gali Michlevich <gali@starkware.co>
2026-01-02 12:28:00 +0200

    Move LogUp Functions

--- crates/stwo-circuits/src/examples/simple_statement.rs
+++ crates/stwo-circuits/src/examples/simple_statement.rs
@@ -9,33 +9,8 @@ use crate::eval;
 use crate::examples::simple_air::{LOG_SIZE_LONG, LOG_SIZE_SHORT};
 use crate::stark_verifier::circle::denom_inverse;
 use crate::stark_verifier::constraint_eval::{CircuitEval, CompositionConstraintAccumulator};
+use crate::stark_verifier::logup::{combine_term, single_logup_term};
 use crate::stark_verifier::statement::{EvaluateArgs, Statement};
-
-/// Computes the denominator of a logup term.
-fn combine_term(
-    context: &mut Context<impl IValue>,
-    element: &[Var],
-    interaction_elements: [Var; 2],
-) -> Var {
-    let mut element_iter = element.iter().rev();
-    let mut value = *element_iter.next().unwrap();
-    for elm in element_iter {
-        value = eval!(context, (value) * (interaction_elements[1]));
-        value = eval!(context, (value) + (*elm));
-    }
-    eval!(context, (value) - (interaction_elements[0]))
-}
-
-/// Computes the constraint polynomial for a single logup term.
-fn single_logup_term(
-    context: &mut Context<impl IValue>,
-    element: &[Var],
-    shifted_diff: Var,
-    interaction_elements: [Var; 2],
-) -> Var {
-    let denominator = combine_term(context, element, interaction_elements);
-    eval!(context, ((shifted_diff) * (denominator)) - (1))
-}
 
 pub struct SimpleStatement {
     pub long_fib_component: SquaredFibonacciComponent,
