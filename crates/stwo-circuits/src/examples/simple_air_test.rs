9eb956993d1c3b0a2404b67dfbf5c9f18fde9f29
Ilya Lesokhin <ilya@starkware.co>
2026-01-15 10:42:12 +0200

    Add enable bit.

--- crates/stwo-circuits/src/examples/simple_air_test.rs
+++ crates/stwo-circuits/src/examples/simple_air_test.rs
@@ -5,13 +5,17 @@ use stwo::core::vcs_lifted::blake2_merkle::Blake2sM31MerkleChannel;
 use stwo::core::verifier::verify;
 
 use crate::examples::simple_air::{LOG_SIZE_LONG, LOG_SIZE_SHORT, PublicInput, create_proof};
-use crate::stark_verifier::proof_from_stark_proof::pack_component_log_sizes;
+use crate::stark_verifier::proof_from_stark_proof::{pack_component_log_sizes, pack_enable_bits};
 
 #[test]
 fn verify_simple_proof() {
     let config = PcsConfig::default();
-    let (components, PublicInput { claimed_sums, component_log_sizes }, _config, proof) =
-        create_proof();
+    let (
+        components,
+        PublicInput { enable_bits, claimed_sums, component_log_sizes },
+        _config,
+        proof,
+    ) = create_proof();
 
     // Verify.
     let verifier_channel = &mut Blake2sM31Channel::default();
@@ -19,7 +23,6 @@ fn verify_simple_proof() {
 
     // Retrieve the expected column sizes in each commitment interaction, from the AIR.
     let sizes = TreeVec::concat_cols(components.iter().map(|c| c.trace_log_degree_bounds()));
-
     let preprocessed_column_sizes = vec![LOG_SIZE_SHORT, LOG_SIZE_LONG];
 
     commitment_scheme.commit(
@@ -27,7 +30,8 @@ fn verify_simple_proof() {
         &preprocessed_column_sizes,
         verifier_channel,
     );
-    verifier_channel.mix_felts(&pack_component_log_sizes(component_log_sizes));
+    verifier_channel.mix_felts(&pack_enable_bits(&enable_bits));
+    verifier_channel.mix_felts(&pack_component_log_sizes(&component_log_sizes));
     commitment_scheme.commit(proof.proof.commitments[1], &sizes[1], verifier_channel);
     verifier_channel.mix_felts(&claimed_sums);
     commitment_scheme.commit(proof.proof.commitments[2], &sizes[2], verifier_channel);
