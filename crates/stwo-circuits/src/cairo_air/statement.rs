826ae9a3c8f8d9c786c28cba8f0e5020dac5b51b
Ilya Lesokhin <ilya@starkware.co>
2026-01-27 17:29:50 +0200

    Get public data as Vec<M31>.

--- crates/stwo-circuits/src/cairo_air/statement.rs
+++ crates/stwo-circuits/src/cairo_air/statement.rs
@@ -3,10 +3,11 @@ use std::collections::HashMap;
 
 use crate::cairo_air::components;
 use crate::cairo_air::components::memory_address_to_id::MEMORY_ADDRESS_TO_ID_RELATION_ID;
-use crate::circuits::ops::div;
+use crate::circuits::ops::{Guess, div};
 use crate::eval;
 use crate::stark_verifier::extract_bits::extract_bits;
 use crate::stark_verifier::logup::combine_term;
+use crate::stark_verifier::proof_from_stark_proof::pack_into_qm31s;
 use itertools::{Itertools, chain, izip};
 use num_traits::Zero;
 use stwo::core::fields::qm31::QM31;
@@ -168,12 +169,12 @@ pub struct CairoStatement<Value: IValue> {
 }
 
 impl<Value: IValue> CairoStatement<Value> {
-    pub fn new(
-        context: &mut Context<Value>,
-        packed_public_data: Vec<Var>,
-        public_data_len: usize,
-    ) -> Self {
-        let packed_public_data = Simd::from_packed(packed_public_data, public_data_len);
+    pub fn new(context: &mut Context<Value>, public_data: Vec<M31>) -> Self {
+        let packed_public_data = pack_into_qm31s(public_data.iter().cloned())
+            .into_iter()
+            .map(|qm31| Value::from_qm31(qm31).guess(context))
+            .collect_vec();
+        let packed_public_data = Simd::from_packed(packed_public_data, public_data.len());
         let unpacked_simd = Simd::unpack(context, &packed_public_data);
 
         let public_data = PublicData::<Var>::parse_from_vars(&unpacked_simd[..]);
