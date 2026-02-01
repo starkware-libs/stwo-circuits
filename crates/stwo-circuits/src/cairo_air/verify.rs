12c970a5f881449cc8a92419af66f362ceecb7a6
Ilya Lesokhin <ilya@starkware.co>
2026-02-01 12:52:43 +0200

    Prove test_prove_verify_all_opcode_components.

--- crates/stwo-circuits/src/cairo_air/verify.rs
+++ crates/stwo-circuits/src/cairo_air/verify.rs
@@ -1,10 +1,7 @@
-use crate::cairo_air::statement::CairoStatement;
-use crate::cairo_air::statement::MEMORY_VALUES_LIMBS;
-use crate::cairo_air::statement::PUB_MEMORY_VALUE_LEN;
-use crate::cairo_air::statement::PUBLIC_DATA_LEN;
+use crate::cairo_air::statement::{CairoStatement, MEMORY_VALUES_LIMBS};
 use crate::circuits::context::{Context, TraceContext};
 use crate::circuits::ops::Guess;
-use crate::stark_verifier::proof::{Claim, Proof, ProofConfig};
+use crate::stark_verifier::proof::{Claim, ProofConfig};
 use crate::stark_verifier::proof_from_stark_proof::{
     pack_component_log_sizes, pack_enable_bits, proof_from_stark_proof,
 };
@@ -12,39 +9,15 @@ use crate::stark_verifier::verify::verify;
 use cairo_air::CairoProof;
 use cairo_air::flat_claims::FlatClaim;
 use cairo_air::flat_claims::flatten_interaction_claim;
-use num_traits::Zero;
+use itertools::Itertools;
+use std::array;
 use stwo::core::fields::m31::M31;
-use stwo::core::fields::qm31::{QM31, SecureField};
+use stwo::core::fields::qm31::QM31;
 use stwo::core::vcs_lifted::blake2_merkle::Blake2sM31MerkleHasher;
 
 /// Circuit Verifies a [CairoProof].
-// TODO(Gali): Add test.
 pub fn verify_cairo(proof: &CairoProof<Blake2sM31MerkleHasher>) -> Context<QM31> {
     let mut context = TraceContext::default();
-
-    let output_len = 1;
-    let program_len = 128;
-    let flat_claim =
-        vec![M31::zero(); PUBLIC_DATA_LEN + output_len * PUB_MEMORY_VALUE_LEN + program_len];
-    let program = vec![[M31::zero(); MEMORY_VALUES_LIMBS]; program_len];
-    let outputs = vec![[M31::zero(); MEMORY_VALUES_LIMBS]; output_len];
-    let statement = CairoStatement::<QM31>::new(&mut context, flat_claim, outputs, program);
-
-    let (config, proof) = proof_from_cairo_proof(proof, &statement);
-
-    let proof_vars = proof.guess(&mut context);
-
-    verify(&mut context, &proof_vars, &config, &statement);
-
-    context
-}
-
-/// Prepares the input for the circuit verifier by converting the [CairoProof] to a
-/// [ProofConfig] and a [Proof].
-pub fn proof_from_cairo_proof(
-    proof: &CairoProof<Blake2sM31MerkleHasher>,
-    statement: &CairoStatement<QM31>,
-) -> (ProofConfig, Proof<SecureField>) {
     let CairoProof {
         claim,
         interaction_pow,
@@ -54,31 +27,51 @@ pub fn proof_from_cairo_proof(
         preprocessed_trace_variant: _,
     } = proof;
 
-    let FlatClaim { component_enable_bits, component_log_sizes, public_data: _ } =
+    let FlatClaim { component_enable_bits, component_log_sizes, public_data } =
         FlatClaim::from_cairo_claim(claim);
 
-    // TODO(ilya): Create the statment based on the public_claim.
+    let (public_claim, outputs, program) = public_data.pack_into_u32s();
 
+    let public_claim = public_claim.iter().map(|u32| M31::from_u32_unchecked(*u32)).collect_vec();
+
+    debug_assert!(outputs.len().is_multiple_of(MEMORY_VALUES_LIMBS));
+    let outputs = outputs
+        .chunks_exact(MEMORY_VALUES_LIMBS)
+        .map(|chunk| array::from_fn(|i| M31::from_u32_unchecked(chunk[i])))
+        .collect_vec();
+    debug_assert!(program.len().is_multiple_of(MEMORY_VALUES_LIMBS));
+    let program = program
+        .chunks_exact(MEMORY_VALUES_LIMBS)
+        .map(|chunk| array::from_fn(|i| M31::from_u32_unchecked(chunk[i])))
+        .collect_vec();
+
+    let statement = CairoStatement::<QM31>::new(&mut context, public_claim, outputs, program);
     let log_trace_size = component_log_sizes.iter().max().unwrap();
     let config = ProofConfig::from_statement(
-        statement,
+        &statement,
         *log_trace_size as usize,
         &proof.extended_stark_proof.proof.config,
     );
-
+    assert!(component_enable_bits.iter().all(|b| *b));
+    assert_eq!(component_enable_bits.len(), config.n_components);
     let component_claimed_sums = flatten_interaction_claim(interaction_claim);
+    assert_eq!(component_claimed_sums.len(), config.n_components);
     let claim = Claim {
         packed_enable_bits: pack_enable_bits(&component_enable_bits),
         packed_component_log_sizes: pack_component_log_sizes(&component_log_sizes),
         claimed_sums: component_claimed_sums,
     };
 
-    let proof = proof_from_stark_proof(
+    let proof_vars = proof_from_stark_proof(
         extended_stark_proof,
         &config,
         claim,
         *interaction_pow,
         *channel_salt,
-    );
-    (config, proof)
+    )
+    .guess(&mut context);
+
+    verify(&mut context, &proof_vars, &config, &statement);
+
+    context
 }
