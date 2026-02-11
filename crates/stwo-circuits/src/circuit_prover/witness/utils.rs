80b0ccee20e95f78441dbeca8d8da72827427225
Ilya Lesokhin <ilya@starkware.co>
2026-02-11 16:59:15 +0200

    Parallel pack_values.

--- crates/stwo-circuits/src/circuit_prover/witness/utils.rs
+++ crates/stwo-circuits/src/circuit_prover/witness/utils.rs
@@ -1,6 +1,11 @@
+use rayon::iter::ParallelIterator;
+use rayon::slice::ParallelSlice;
 use stwo::prover::backend::simd::conversion::Pack;
 use stwo::prover::backend::simd::m31::N_LANES;
 
-pub fn pack_values<T: Pack + Copy>(values: &[T]) -> Vec<T::SimdType> {
-    values.chunks_exact(N_LANES).map(|c| T::pack(c.try_into().unwrap())).collect()
+pub fn pack_values<T: Pack + Copy + Sync>(values: &[T]) -> Vec<T::SimdType>
+where
+    T::SimdType: Send,
+{
+    values.par_chunks_exact(N_LANES).map(|c| T::pack(c.try_into().unwrap())).collect()
 }
