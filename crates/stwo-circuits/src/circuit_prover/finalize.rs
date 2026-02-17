d1a0a689988e03c57efded4cc14decf26d74478c
Alon F <alonf@starkware.co>
2026-02-17 14:56:23 +0200

    wip

--- crates/stwo-circuits/src/circuit_prover/finalize.rs
+++ crates/stwo-circuits/src/circuit_prover/finalize.rs
@@ -37,27 +37,24 @@ fn pad_eq(context: &mut Context<impl IValue>) {
 }
 
 fn pad_blake(context: &mut Context<impl IValue>) {
-    let n_blake_gates = context.circuit.blake.len();
-    assert_ne!(
-        std::cmp::max(n_blake_gates.next_power_of_two(), N_LANES),
-        n_blake_gates,
-        "We want padding."
-    );
-    // The number of rows in blake output component is equal to the number of blake gates in the
-    // circuit.
-    let blake_output_padding =
-        std::cmp::max(n_blake_gates.next_power_of_two(), N_LANES) - n_blake_gates;
+    // The number of rows in blake output component is equal to the number of blake gates.
+    let n_blake_output_rows = context.circuit.blake.len();
+    let target_blake_output_rows = std::cmp::max(n_blake_output_rows.next_power_of_two(), N_LANES);
+    let blake_output_padding = target_blake_output_rows - n_blake_output_rows;
     let zero = context.zero();
-    // let leet = crate::circuits::ops::guess(context, qm31_from_u32s(1337, 1337, 1337, 1337));
-    // let leet = context.constant(qm31_from_u32s(1337, 1337, 1337, 1337));
-    for _ in 0..blake_output_padding - 1 {
+
+    // Reserve one final gate for aligning both blake output and blake compress row counts.
+    let n_single_block_padding_gates = blake_output_padding - 1;
+    for _ in 0..n_single_block_padding_gates {
         crate::circuits::blake::blake(context, &[zero], 1);
     }
-    let n_blake_compress: usize = context.circuit.blake.iter().map(|gate| gate.input.len()).sum();
-    let blake_compress_padding =
-        std::cmp::max(n_blake_compress.next_power_of_two(), N_LANES) - n_blake_compress;
-    let n_last = blake_compress_padding * 4;
-    crate::circuits::blake::blake(context, &vec![zero; n_last], n_last * 16);
+
+    let n_blake_compress_rows: usize = context.circuit.blake.iter().map(|gate| gate.input.len()).sum();
+    let target_blake_compress_rows =
+        std::cmp::max(n_blake_compress_rows.next_power_of_two(), N_LANES);
+    let blake_compress_padding = target_blake_compress_rows - n_blake_compress_rows;
+    let n_last_words = blake_compress_padding * 4;
+    crate::circuits::blake::blake(context, &vec![zero; n_last_words], n_last_words * 16);
 }
 fn hash_constants(context: &mut Context<impl IValue>) -> HashValue<Var> {
     let constants: Vec<_> = context.constants().values().copied().collect();
