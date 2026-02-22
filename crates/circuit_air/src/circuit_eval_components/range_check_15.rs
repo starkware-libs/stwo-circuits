fe650be9d0ce44130a345f1fa36fd4fb3956195a
Alon F <alonf@starkware.co>
2026-02-22 18:23:29 +0200

    wip

---
+++ crates/circuit_air/src/circuit_eval_components/range_check_15.rs
@@ -0,0 +1,53 @@
+// This file was created by the AIR team.
+
+use super::prelude::*;
+
+pub const N_TRACE_COLUMNS: usize = 1;
+pub const N_INTERACTION_COLUMNS: usize = 4;
+
+pub const RELATION_USES_PER_ROW: [RelationUse; 0] = [];
+
+#[allow(unused_variables)]
+pub fn accumulate_constraints<Value: IValue>(
+    input: &[Var],
+    context: &mut Context<Value>,
+    component_data: &dyn ComponentDataTrait<Value>,
+    acc: &mut CompositionConstraintAccumulator,
+) {
+    let [multiplicity_0_col0] = input.try_into().unwrap();
+    let seq_15 = acc.get_preprocessed_column(&PreProcessedColumnId { id: "seq_15".to_owned() });
+
+    // Yield RangeCheck_15.
+    let tuple_0 = &[eval!(context, 1058718565), eval!(context, seq_15)];
+    let numerator_0 = eval!(context, -(multiplicity_0_col0));
+    acc.add_to_relation(context, numerator_0, tuple_0);
+}
+
+pub struct Component {}
+impl<Value: IValue> CircuitEval<Value> for Component {
+    fn name(&self) -> String {
+        "range_check_15".to_string()
+    }
+
+    fn evaluate(
+        &self,
+        context: &mut Context<Value>,
+        component_data: &dyn ComponentDataTrait<Value>,
+        acc: &mut CompositionConstraintAccumulator,
+    ) {
+        accumulate_constraints(component_data.trace_columns(), context, component_data, acc);
+        // Verify this component has 2 ** 15 rows
+    }
+
+    fn trace_columns(&self) -> usize {
+        N_TRACE_COLUMNS
+    }
+
+    fn interaction_columns(&self) -> usize {
+        N_INTERACTION_COLUMNS
+    }
+
+    fn relation_uses_per_row(&self) -> &[RelationUse] {
+        &RELATION_USES_PER_ROW
+    }
+}
