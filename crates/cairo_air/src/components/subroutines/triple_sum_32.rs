e4ecbc3a0bbbb0e9771066deee9675494c3776e0
Stav Beno <stav@starkware.co>
2026-03-26 16:45:12 +0200

    integrate verify triple sum

--- crates/cairo_air/src/components/subroutines/triple_sum_32.rs
+++ crates/cairo_air/src/components/subroutines/triple_sum_32.rs
@@ -22,37 +22,20 @@ pub fn accumulate_constraints<Value: IValue>(
         triple_sum32_res_limb_1_col1,
     ] = input.try_into().unwrap();
 
-    let carry_low_tmp_541fa_1 = eval!(
+    verify_triple_sum_32::accumulate_constraints(
+        &[
+            eval!(context, triple_sum_32_input_a_limb_0),
+            eval!(context, triple_sum_32_input_a_limb_1),
+            eval!(context, triple_sum_32_input_b_limb_0),
+            eval!(context, triple_sum_32_input_b_limb_1),
+            eval!(context, triple_sum_32_input_c_limb_0),
+            eval!(context, triple_sum_32_input_c_limb_1),
+            eval!(context, triple_sum32_res_limb_0_col0),
+            eval!(context, triple_sum32_res_limb_1_col1),
+        ],
         context,
-        ((((triple_sum_32_input_a_limb_0) + (triple_sum_32_input_b_limb_0))
-            + (triple_sum_32_input_c_limb_0))
-            - (triple_sum32_res_limb_0_col0))
-            * (32768)
+        component_data,
+        acc,
     );
-
-    //carry low is 0 or 1 or 2.
-    let constraint_1_value = eval!(
-        context,
-        ((carry_low_tmp_541fa_1) * ((carry_low_tmp_541fa_1) - (1)))
-            * ((carry_low_tmp_541fa_1) - (2))
-    );
-    acc.add_constraint(context, constraint_1_value);
-
-    let carry_high_tmp_541fa_2 = eval!(
-        context,
-        (((((triple_sum_32_input_a_limb_1) + (triple_sum_32_input_b_limb_1))
-            + (triple_sum_32_input_c_limb_1))
-            + (carry_low_tmp_541fa_1))
-            - (triple_sum32_res_limb_1_col1))
-            * (32768)
-    );
-
-    //carry high is 0 or 1 or 2.
-    let constraint_3_value = eval!(
-        context,
-        ((carry_high_tmp_541fa_2) * ((carry_high_tmp_541fa_2) - (1)))
-            * ((carry_high_tmp_541fa_2) - (2))
-    );
-    acc.add_constraint(context, constraint_3_value);
     vec![]
 }
