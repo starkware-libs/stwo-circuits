cfba761eb3722e58b2df73144b077d25b3bc9fa3
Gali Michlevich <gali@starkware.co>
2026-03-31 11:32:58 +0300

    Remove Unnecessary Gates

--- crates/circuits/src/simd.rs
+++ crates/circuits/src/simd.rs
@@ -247,8 +247,7 @@ impl Simd {
     pub fn pow2(context: &mut Context<impl IValue>, bits: &[Simd]) -> Simd {
         let len = bits[0].len();
         let mut res = Simd::one(context, len);
-        let one = context.one();
-        let mut pow2 = M31Wrapper::new_unsafe(eval!(context, (one) + (one)));
+        let mut pow2 = M31Wrapper::new_unsafe(context.constant(2.into()));
         for (bit_idx, bit) in bits.iter().enumerate() {
             let res_if_bit_is_one = Simd::scalar_mul(context, &res, &pow2);
             // Select between `res` and `res_if_bit_is_one` based on the value of the bit.
@@ -268,7 +267,7 @@ impl Simd {
     pub fn combine_bits(context: &mut Context<impl IValue>, bits: &[Simd]) -> Simd {
         let mut iter = bits.iter().rev();
         let mut res = iter.next().unwrap().clone();
-        let two = M31Wrapper::new_unsafe(eval!(context, context.constant(QM31::from(2))));
+        let two = M31Wrapper::new_unsafe(context.constant(2.into()));
         for bit in iter {
             res = Simd::scalar_mul(context, &res, &two);
             res = Simd::add(context, &res, bit);
