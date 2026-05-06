5328232a930b7cc73c3cd15f003c6cc186b0e38e
Stavbe <stav@starkware.co>
2026-05-06 12:45:47 +0300

    add blake in circuit + tests (#462)

--- crates/circuit_verifier/src/blake2s_consts.rs
+++ crates/circuit_verifier/src/blake2s_consts.rs
@@ -1,6 +1,4 @@
-pub const BLAKE2S_IV: [u32; 8] = [
-    0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19,
-];
+pub use circuits::blake::BLAKE2S_IV;
 
 pub fn blake2s_initial_state() -> [u32; 8] {
     let mut h = BLAKE2S_IV;
