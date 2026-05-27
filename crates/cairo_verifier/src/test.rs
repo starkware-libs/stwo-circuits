b8fa9b197b36174c28fcfac8e42948857ddfe04a
leo-starkware <leo@starkware.co>
2026-05-27 17:01:46 +0300

    Test multiverifier of two cairo proofs.

--- crates/cairo_verifier/src/test.rs
+++ crates/cairo_verifier/src/test.rs
@@ -1,4 +1,3 @@
-use std::array;
 use std::collections::HashSet;
 use std::fs::{File, OpenOptions};
 use std::sync::Arc;
@@ -12,12 +11,9 @@ use circuit_common::N_RESERVED;
 use circuits::context::{Context, FinalizedContext};
 use circuits::ivalue::NoValue;
 use circuits::ops::Guess;
-use circuits_stark_verifier::constraint_eval::CircuitEval;
 use circuits_stark_verifier::proof::{ProofConfig, empty_proof};
 use circuits_stark_verifier::statement::Statement;
 use circuits_stark_verifier::verify::verify;
-use itertools::Itertools;
-use itertools::zip_eq;
 use num_traits::Zero;
 use stwo::core::fields::m31::M31;
 use stwo::core::fields::qm31::QM31;
@@ -33,10 +29,7 @@ use crate::all_components::all_components;
 use crate::preprocessed_columns::MAX_SEQUENCE_LOG_SIZE;
 use crate::statement::{CairoStatement, MEMORY_VALUES_LIMBS, PUBLIC_DATA_LEN};
 use crate::utils::get_proof_file_path;
-use crate::verify::{
-    CairoVerifierConfig, enabled_components, get_preprocessed_root,
-    prepare_cairo_proof_for_circuit_verifier, verify_fixed_cairo_circuit,
-};
+use crate::verify::{enabled_components, get_preprocessed_root, verify_cairo_with_component_set};
 
 /// Circuit Verifies a [CairoProof].
 pub fn verify_cairo(
@@ -49,62 +42,6 @@ pub fn verify_cairo(
         enabled_components::<QM31>(&component_enable_bits).into_keys().collect();
 
     verify_cairo_with_component_set(proof, components)
-}
-
-/// Verifies a [CairoProof] with a given set of components.
-pub fn verify_cairo_with_component_set(
-    cairo_proof: &CairoProof<Blake2sM31MerkleHasher>,
-    component_set: HashSet<&str>,
-) -> Result<FinalizedContext<QM31>, String> {
-    let FlatClaim { component_enable_bits, component_log_sizes, public_data: _ } =
-        cairo_proof.claim.flatten_claim();
-    let components: indexmap::IndexMap<&'static str, Box<dyn CircuitEval<QM31>>> =
-        zip_eq(all_components::<QM31>().into_iter(), &component_enable_bits)
-            .filter_map(|((component_name, component), &enable_bit)| {
-                let component_in_set = component_set.contains(component_name);
-                if component_in_set != enable_bit {
-                    return Some(Err(format!(
-                        "Proof was produced with the wrong components set: expected the component '{}' to be {} according to the component set, but it is {} in the proof.",
-                        component_name,
-                        if component_in_set { "enabled" } else { "disabled" },
-                        if enable_bit { "enabled" } else { "disabled" }
-                    )));
-                }
-                if enable_bit { Some(Ok((component_name, component))) } else { None }
-            })
-            .try_collect()?;
-
-    let proof_config = ProofConfig::new(
-        &components,
-        cairo_proof.preprocessed_trace_variant.n_columns(),
-        &cairo_proof.extended_stark_proof.proof.config,
-        INTERACTION_POW_BITS,
-    );
-
-    let (proof, public_data) =
-        prepare_cairo_proof_for_circuit_verifier(cairo_proof, &component_enable_bits);
-    let (mut public_claim, outputs, program) = public_data.pack_into_u32s();
-    public_claim.extend(component_log_sizes);
-    let outputs = outputs
-        .chunks_exact(MEMORY_VALUES_LIMBS)
-        .map(|chunk| array::from_fn(|i| M31::from_u32_unchecked(chunk[i])))
-        .collect_vec();
-    let program = program
-        .chunks_exact(MEMORY_VALUES_LIMBS)
-        .map(|chunk| array::from_fn(|i| M31::from_u32_unchecked(chunk[i])))
-        .collect();
-
-    let ppt_root = cairo_proof.extended_stark_proof.proof.commitments[0];
-    let verifier_config = CairoVerifierConfig {
-        preprocessed_root: ppt_root.into(),
-        proof_config,
-        enabled_bits: component_enable_bits,
-        program,
-        n_outputs: cairo_proof.claim.public_data.public_memory.output.len(),
-        preprocessed_trace_variant: cairo_proof.preprocessed_trace_variant,
-    };
-
-    verify_fixed_cairo_circuit(&verifier_config, proof, public_claim, outputs)
 }
 
 #[test]
